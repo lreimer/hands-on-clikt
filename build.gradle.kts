ba58a0be855d772fb0b741caad01dba85ef34c02
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2021-03-26 13:39:14 +0100

    Implemented simple CLI application with native image build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,44 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    implementation("com.github.ajalt.clikt:clikt:3.1.0")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 application {
     mainClass.set("hands.on.clikt.AppKt")
+}
+
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
+
+val copyRuntimeLibs = task<Copy>("copyRuntimeLibs") {
+    into("$buildDir/libs")
+    from(configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") })
+}
+
+tasks.getByName("assemble").finalizedBy(copyRuntimeLibs)
+
+fun getClasspath() = "libs/" + file("$buildDir/libs").list()?.joinToString(":libs/")
+
+task<Exec>("buildNativeImage") {
+    description = "Build native image using GraalVM"
+    dependsOn(tasks.getByName("build"))
+    workingDir(buildDir)
+    commandLine(
+        "native-image",
+        "-cp", getClasspath(),
+        "-H:+ReportExceptionStackTraces",
+        "-H:+AddAllCharsets",
+        // "--report-unsupported-elements-at-runtime",
+        // "--allow-incomplete-classpath",
+        "--no-server",
+        "--no-fallback",
+        "--enable-http",
+        "--enable-https",
+        "hands.on.clikt.AppKt",
+        "hands-on-clikt"
+    )
 }
--- src/main/kotlin/hands/on/clikt/App.kt
+++ src/main/kotlin/hands/on/clikt/App.kt
@@ -1,5 +1,11 @@
 package hands.on.clikt
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.parameters.types.int
+
 class App {
     val greeting: String
         get() {
@@ -7,6 +13,17 @@ class App {
         }
 }
 
-fun main() {
-    println(App().greeting)
+class Hello : CliktCommand() {
+    val count: Int by option(help="Number of greetings").int().default(1)
+
+    val name: String by option(help="The person to greet").prompt("Your name")
+
+    override fun run() {
+        repeat(count) {
+            echo("Hello $name!")
+        }
+    }
 }
+
+fun main(args: Array<String>) = Hello().main(args)
+
